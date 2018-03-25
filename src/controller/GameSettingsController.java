1100f8956b4e01cd18bb8090fbcaee71d7a0c9f6
wzd833 <burnerpower@hotmail.com>
2018-03-25 01:59:42 -0500

    Added placeholder gamesettingscontroller

---
+++ src/controller/GameSettingsController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class GameSettingsController implements Initializable, GeneralController {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
