14e7d4e4ff6c1225e2b78656dd0aa0bf4b2d80a6
wzd833 <burnerpower@hotmail.com>
2018-03-25 17:32:59 -0500

    Added some more MatchPage functionality

--- src/controller/MatchPageController.java
+++ src/controller/MatchPageController.java
@@ -8,6 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import utility.ViewManager;
 
@@ -18,7 +19,7 @@ public class MatchPageController implements Initializable, GeneralController {
 	private boolean villager;
 	private boolean police;
 	
-	@FXML private ChoiceBox<Integer> maxPlayers;
+	@FXML private ChoiceBox<String> maxPlayers;
 	
 	@FXML private void CreateMatchClicked(ActionEvent event) throws IOException {
 		try {
@@ -29,35 +30,49 @@ public class MatchPageController implements Initializable, GeneralController {
 
 	}
 	
+	//returns 0 if there is no max, returns the number of max players otherwise
+	@FXML private int getMaxPlayers() {
+		if(maxPlayers.getValue().equals("None")) {
+			return 0;
+		}
+		else {
+			return Integer.parseInt(maxPlayers.getValue());
+		}
+			
+	}
+	
 	@FXML private void MatchSearchClicked(ActionEvent event) throws IOException {
 		//Should check database for available matches and search based on filters
 	}
 	
-	@FXML private void setDetective(ActionEvent event) throws IOException {
-		detective=!detective;
-		System.out.println(event.getTarget());
-	}
-	
-	@FXML private void setMafia(ActionEvent event) throws IOException {
-		mafia=!mafia;
-		System.out.println(event.getTarget());
-	}
-	
-	@FXML private void setVillager(ActionEvent event) throws IOException {
-		villager=!villager;
-	}
-	
-	@FXML private void setPolice(ActionEvent event) throws IOException {
-		police=!police;
+	@FXML private void setCheckBox(ActionEvent event) throws IOException {
+		//Casts the object in getSource to a CheckBox then calls the getText method of Checkbox to get the case
+		String s= ((CheckBox) event.getSource()).getText();
+		switch(s) {
+			case "Detective":
+				detective=!detective;
+				break;
+			case "Mafia":
+				mafia=!mafia;
+				break;
+			case "Police":
+				police=!police;
+				break;
+			case "Villager":
+				villager=!villager;
+				break;
+		}
+		
 	}
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		maxPlayers.setItems(FXCollections.observableArrayList(3,4,5,6,7,8));
+		maxPlayers.setItems(FXCollections.observableArrayList("None","3","4","5","6","7","8"));
+		maxPlayers.getSelectionModel().selectFirst();
 		detective=false;
 		mafia=false;
 		villager=false;
-		police=true;
+		police=false;
 	}
 
 }
