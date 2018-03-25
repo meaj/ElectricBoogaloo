e479b7f3d7c4cf879f36ff2913fe3a3fb8fa5486
tgg12321 <tgg12321@gmail.com>
2018-03-25 01:14:33 -0500

    Fixing chases sins

--- src/controller/LobbyHostController.java
+++ src/controller/LobbyHostController.java
@@ -3,13 +3,36 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import utility.ViewManager;
 
 public class LobbyHostController implements Initializable {
 
+	@FXML private TextField chatTextField;
+	@FXML private Button startGameButton;
+	@FXML private Button signupPassword;
+	
+	
+	public LobbyHostController(){
+	}
+	
+	@FXML void startGameButtonClicked(){
+		try {
+			ViewManager.getInstance().changeView(ViewManager.MATCH_PLAYER, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML void settingsButtonClicked(){
+		
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
 	}
 
