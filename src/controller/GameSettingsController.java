2786daf2aeb08067d67b9c4084b022b705006cc7
tgg12321 <tgg12321@gmail.com>
2018-03-25 19:42:42 -0500

    Game settings changing

--- src/controller/GameSettingsController.java
+++ src/controller/GameSettingsController.java
@@ -1,13 +1,26 @@
 package controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import utility.ViewManager;
 
-public class GameSettingsController extends SettingsController implements Initializable, GeneralController {
+public class GameSettingsController implements Initializable, GeneralController {
+
+	@FXML private TextField villagersTextField;
+	@FXML private TextField detectiveTextField;
+	@FXML private TextField doctorTextField;
+	@FXML private TextField vampireTextField;
+	@FXML private ChoiceBox<Integer> maxPlayers;
+	private int totalPlayers =8;
+	private int numVillagers, numDetectives, numDoctors, numVampires;
+	
 	@FXML private void saveButtonClicked(ActionEvent event) throws IOException {
 		try {
 			ViewManager.getInstance().changeView(ViewManager.LOBBY_HOST, null);
@@ -15,6 +28,20 @@ public class GameSettingsController extends SettingsController implements Initia
 			e.printStackTrace();
 		}
 
+	}
+	
+	private int getSumRoles(){
+		numVillagers = Integer.parseInt(villagersTextField.getText());
+		numDetectives +=Integer.parseInt(detectiveTextField.getText());
+		numDoctors+=Integer.parseInt(doctorTextField.getText());
+		numVampires +=Integer.parseInt(vampireTextField.getText());
+		return numVillagers + numDetectives +numDoctors + numVampires;
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		maxPlayers.setValue(totalPlayers);
+		
 	}
 
 }
--- src/controller/RulesController.java
+++ src/controller/RulesController.java
@@ -1,14 +1,9 @@
 package controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import utility.ViewManager;
 
 public class RulesController implements Initializable, GeneralController {
 	
