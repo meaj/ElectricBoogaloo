d33d721a2b7cb4bc76c30f822550b584f8707610
kcs805 <kcs805@N0000A107.cs.utsarr.net>
2018-03-25 20:30:20 -0500

    Alerts for game settings

--- src/controller/GameSettingsController.java
+++ src/controller/GameSettingsController.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
+
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import model.User;
+import utility.AlertHelper;
 import utility.ViewManager;
 
 public class GameSettingsController implements Initializable, GeneralController {
@@ -26,13 +30,32 @@ public class GameSettingsController implements Initializable, GeneralController 
 	@FXML private TextField detectiveTextField;
 	@FXML private TextField doctorTextField;
 	@FXML private TextField vampireTextField;
-	@FXML private ChoiceBox<Integer> maxPlayers;
+	@FXML private ChoiceBox<String> maxPlayers;
 	private int totalPlayers =8;
 	private int numVillagers, numDetectives, numDoctors, numVampires;
-
+	private String error;
 	
 	@FXML private void saveButtonClicked(ActionEvent event) throws IOException {
 		try {
+			int sum = getSumRoles();
+			if(sum > totalPlayers){
+				AlertHelper.showWarningMessage("Error", 
+						"Too many roles selected", 
+						"Please select roles equal to max players");
+				 return;
+			}
+			if(sum < totalPlayers){
+				AlertHelper.showWarningMessage("Error", 
+						"Too few roles selected", 
+						"Please select roles equal to max players");
+				 return;
+			}
+			if(numVampires < 1){
+				AlertHelper.showWarningMessage("Error", 
+						"Too few vampires selected", 
+						"Please select at least one vampire role");
+				 return;
+			}
 			ViewManager.getInstance().changeView(ViewManager.LOBBY_HOST, null);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -50,7 +73,12 @@ public class GameSettingsController implements Initializable, GeneralController 
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		maxPlayers.setValue(totalPlayers);
+		maxPlayers.setItems(FXCollections.observableArrayList("None","3","4","5","6","7","8"));
+		maxPlayers.getSelectionModel().selectLast();
+		villagersTextField.setText("0");
+		detectiveTextField.setText("0");
+		doctorTextField.setText("0");
+		vampireTextField.setText("0");
 		
 	}
 
