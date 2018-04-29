10a4231c17363e147369fa7facc8de15f8782cc5
CHouse013 <CHouse013@gmail.com>
2018-04-28 19:17:55 -0500

    Hardcoded DB connections

--- src/utility/ConnectionFactory.java
+++ src/utility/ConnectionFactory.java
@@ -17,20 +17,24 @@ public class ConnectionFactory {
 		//read properties file
 		try {
     		Properties props = new Properties();
-    		FileInputStream fis = new FileInputStream("db.properties");
-	        props.load(fis);
-	        fis.close();
+    		//FileInputStream fis = new FileInputStream("db.properties");
+	        //props.load(fis);
+	        //fis.close();
 
 	        //create the datasource
 	        MysqlDataSource ds = new MysqlDataSource();
-	        ds.setURL(props.getProperty("MYSQL_DB_URL"));
-	        ds.setUser(props.getProperty("MYSQL_DB_USERNAME"));
-	        ds.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+	        ds.setURL(props.getProperty("jdbc:mysql://easel2.fulgentcorp.com:3306/lgy513"));
+	        ds.setUser(props.getProperty("lgy513"));
+	        ds.setPassword(props.getProperty("password"));
 			
 			//create connection
 	        conn = ds.getConnection();
 	        
-		} catch (IOException | SQLException e) {
+		/*} catch (IOException | SQLException e) {
+			e.printStackTrace();
+			throw new Exception(e);
+		} */
+		} catch(SQLException e) {
 			e.printStackTrace();
 			throw new Exception(e);
 		}
